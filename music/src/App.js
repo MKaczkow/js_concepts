67615e15fef3416b75fdc82379342059d395f9b9
Kaczkowski Maciej <Maciej.Kaczkowski@orange.com>
2022-01-04 16:42:58 +0100

    music app init, timestamp is 4:00, part #1

---
+++ music/src/App.js
@@ -0,0 +1,39 @@
+import {useState} from 'react';
+
+function App() {
+
+const [songs,setSongs] = useState([
+  {
+    title: mock,
+    artist: mock,
+    img_src: mock,
+    src: mock,
+  },
+  {
+    title: mock,
+    artist: mock,
+    img_src: mock,
+    src: mock,
+  },
+  {
+    title: mock,
+    artist: mock,
+    img_src: mock,
+    src: mock,
+  },
+  {
+    title: mock,
+    artist: mock,
+    img_src: mock,
+    src: mock,
+  }
+])
+
+  return (
+    <div className="app">
+      
+    </div>
+  );
+}
+
+export default App;
