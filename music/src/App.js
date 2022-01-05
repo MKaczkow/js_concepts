9adbaf756576cec985beba488e86f48b4a7ef111
Kaczkowski Maciej <Maciej.Kaczkowski@orange.com>
2022-01-05 14:07:05 +0100

    working on music

--- music/src/App.js
+++ music/src/App.js
@@ -1,39 +1,52 @@
 import {useState} from 'react';
+import Player from './components/Player';
+
 
 function App() {
 
-const [songs,setSongs] = useState([
-  {
-    title: mock,
-    artist: mock,
-    img_src: mock,
-    src: mock,
-  },
-  {
-    title: mock,
-    artist: mock,
-    img_src: mock,
-    src: mock,
-  },
-  {
-    title: mock,
-    artist: mock,
-    img_src: mock,
-    src: mock,
-  },
-  {
-    title: mock,
-    artist: mock,
-    img_src: mock,
-    src: mock,
-  }
-])
+  const [songs,setSongs] = useState([
+    {
+      title: "Forget me too ft. Halsey",
+      artist: "Machine Gun Kelly",
+      img_src: "./images/song-1.jpg",
+      src: ".music/on-n-on.mp3",
+    },
+    {
+      title: "Song 2",
+      artist: "Artist 2",
+      img_src: "./images/song-2.jpg",
+      src: ".music/somebody-new.mp3",
+    },
+    {
+      title: "Song 3",
+      artist: "Artist 2",
+      img_src: "./images/song-3.jpg",
+      src: ".music/on-n-on.mp3",
+    },
+    {
+      title: "Song 4",
+      artist: "Artist 2",
+      img_src: "./images/song-4.jpg",
+      src: ".music/on-n-on.mp3",
+    }
+  ])
+
+    return (
+      <div className="app">
+        
+      </div>
+    );
+
+
+  const [currentSongIndex, setCurrentSongIndex] = useState(0);
+  const [nextSongIndex, setNextSongIndex] = useState(currentSongIndex + 1);
 
   return (
     <div className="app">
-      
+
     </div>
   );
+
 }
 
 export default App;
---
+++ music/src/components/Player.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+function Player() {
+    return (
+        <div className="c-player">
+            <audio></audio>
+            <h4> Playing now </h4>
+            { /* DETAILS */}
+            { /* CONTROLS */}
+            <p><strong> Next up: </strong> Kindest Regards by Lorca</p>
+        </div>
+    )
+}
+
+
+export default Player
