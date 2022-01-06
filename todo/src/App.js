bf81329d4f500096749b46ca0943c07d607f9043
Maciej Kaczkowski <Maciej.Kaczkowski.stud@pw.edu.pl>
2022-01-06 19:34:13 +0100

    working on todo: adding works; deleting and editing doesn't work

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,10 +1,12 @@
+import React from 'react';
 import './App.css';
+import TodoList from './components/TodoList';
 
 
 function App() {
   return (
     <div className="todo-app">
-      <h1> TODO app </h1>
+      <TodoList />
     </div>
   );
 }
