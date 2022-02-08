43fea53702520373e93cf4e3eeb1b37596940e8c
Kaczkowski Maciej <Maciej.Kaczkowski@orange.com>
2022-02-08 08:16:23 +0100

    cleanup - leaflet_maps - without printing

--- README.md
+++ README.md
@@ -5,4 +5,6 @@ repo for basic exercises and very simple apps with React library
 1. Calculator - based on https://bit.ly/3JviUK2
 2. Weather - based on https://bit.ly/3FQeOtW  
 3. Music - based on https://bit.ly/3n19Ans
-4. TODO - based on https://bit.ly/3G0vPBN 
+4. TODO - based on https://bit.ly/3G0vPBN  
+5. Maps - based on https://bit.ly/3gvylow  
+
--- leaflet_maps/src/App.js
+++ leaflet_maps/src/App.js
@@ -1,9 +1,6 @@
-import { Map, TileLayer, withLeaflet } from 'react-leaflet';
-import PrintControl from 'react-leaflet-easyprint';
-
+import { Map, TileLayer } from 'react-leaflet';
 import './App.css';
 
-//const PrintControl = withLeaflet(PrintControlDefault);
 
 function App() {
   return (
@@ -11,15 +8,13 @@ function App() {
       <Map center={[52.00, -21.00]} zoom={7}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          //url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          url="http://10.92.152.30:8080/tile/{z}/{x}/{y}.png"
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          //url="http://10.92.152.30:8080/tile/{z}/{x}/{y}.png"
         />
-      
-        <PrintControl ref={(ref) => { this.printControl = ref; }} position="topleft" sizeModes={['Current', 'A4Portrait', 'A4Landscape']} hideControlContainer={false} />
-        <PrintControl position="topleft" sizeModes={['Current', 'A4Portrait', 'A4Landscape']} hideControlContainer={false} title="Export as PNG" exportOnly />
       </Map>
     </div>
   );
 }
+
 
 export default App;
